3b7ca9a6e4a4e7b2dcc762093cd3bf099623a055
Wisuwat Sunhem <wisuwat_sunhem@hotmail.com>
2014-10-22 15:42:15 +0700

    test class2

--- laravel/app/models/Users.php
+++ laravel/app/models/Users.php
@@ -7,56 +7,56 @@
 		private $name;
 		private $surname;
 		private $status;
-		public function getMaxId(){
+		public static function getMaxId(){
 			return UsersRepository::table('id')->order_by('id', 'desc')->first();
 		}
 		public function __construct( ) {
 			
-   			this->$id;
-			this->$username;
-			this->$password;
-			this->$title;
-			this->$name;
-			this->$surname;
-			this->$status;
+   			$this->$id;
+			$this->$username;
+			$this->$password;
+			$this->$title;
+			$this->$name;
+			$this->$surname;
+			$this->$status;
     	}
 		public function getID(){
-			return this->ID;
+			return $this->ID;
 		}
 		public function setUser($user){
-			this->username = $user;
+			$this->username = $user;
 		}
 		public function getUser(){
-			return this->username;
+			return $this->username;
 		}
 		public function setPass($pass){
-			this->password = $pass;
+			$this->password = $pass;
 		}
 		public function getPass(){
-			return this->password;
+			return $this->password;
 		}
 		public function setTitle($title){
-			this->title = $title;
+			$this->title = $title;
 		}
 		public function getTitle(){
-			return this->title;
+			return $this->title;
 		}
 		public function setName($name){
-			this->name = $name;
+			$this->name = $name;
 		}
 		public function getName(){
-			return this->name;
+			return $this->name;
 		}
 		public function setSurname($surname){
-			this->surname = $surname;
+			$this->surname = $surname;
 		}
 		public function getSurname(){
-			return this->surname;
+			return $this->surname;
 		}
 		public function setStatus($status){
-			this->status = $status;
+			$this->status = $status;
 		}
 		public function getStatus(){
-			return this->status;
+			return $this->status;
 		}
 	}

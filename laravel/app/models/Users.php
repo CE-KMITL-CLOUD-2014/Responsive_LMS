d4a20d34162cd297af17dc107fe68dc6b741f6e1
Wisuwat Sunhem <wisuwat_sunhem@hotmail.com>
2014-10-22 16:45:33 +0700

    test get ID

--- laravel/app/models/Users.php
+++ laravel/app/models/Users.php
@@ -18,7 +18,7 @@
 		}
 		public static function getFromId($id){
 			$dataTmp = UsersRepository::where('ID','=',$id)->get();
-			$obj = new Users;
+			$obj = new Users();
 			/*if(count($dataTmp)!=0){
 				$this->$id=$dataTmp->ID;
 				$this->$username=$dataTmp->username;
